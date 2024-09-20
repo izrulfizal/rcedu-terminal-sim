e6c0ff65babcc640507eac688880a95e37d0df6a
izrulfizal <izrulfizal1410@gmail.com>
2024-09-20 15:58:26 +0800

    Update

--- components/Terminal.vue
+++ components/Terminal.vue
@@ -3,7 +3,9 @@
   </template>
   
   <script>
-  import { Terminal } from '@xterm/xterm';
+  // import { Terminal } from '@xterm/xterm';
+  import pkg from '@xterm/xterm';
+  const { Terminal } = pkg;
   import '@xterm/xterm/css/xterm.css'; 
   
   export default {
